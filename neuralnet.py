2d74469b3d70bb21b09e5de2e9c9a4152c00d492
Nelson <nxc708@miami.edu>
2021-08-23 20:42:01 -0400

    08/23/2021 Functional back propagation added.
    To-do:
    - Implementation example
    - Illustrate network parameter updates
    - Report network accuracy and other statistics
    - Add complex network configuration options

--- neuralnet.py
+++ neuralnet.py
@@ -7,31 +7,33 @@ def sigmoid(x):
     return y
 
 def relu(x):
-    if (x < 0):
-        y = 0
-    else:
-        y = x
-    return y
+    return np.maximum(0, x)
 
 class neuron:
     def __init__(self, inpWeights = 0, bias = 0):
         self.inpWeights = inpWeights
         self.bias = bias
         self.output = 0
+        self.delta = 0
 
     def modWeights(self, weightCnt):
         self.inpWeights = np.random.rand(weightCnt)
+
+
         
 class layer:
-    def __init__(self, length, prevLength = 0):
+    def __init__(self, length, prevLength = 0, activation = sigmoid):
         self.length = length
+        self.activation = activation
         self.neurons = []
         for i in range(length):
             self.neurons.append(neuron(np.random.rand(prevLength)))
 
 class network:
     def __init__(self, hiddenLayers, layerLength, outputCnt):
+        self.hiddenLayers = hiddenLayers
         self.layers = []
+        self.layerLength = layerLength
         for i in range(hiddenLayers):
             if (i == 0):
                 self.layers.append(layer(layerLength))
@@ -43,42 +45,100 @@ class network:
         print(self.layers)
 
     def initData(self, data):
-        self.initialized = True
         for i in range(self.firstLayer.length):
             self.firstLayer.neurons[i].modWeights(len(data))
+        self.initialized = True
+        print("Network intialized.")
 
     def fwdProp(self, dataPoint):
-        for layerIdx, layer in enumerate(self.layers):
-            for neuronIdx, neuron in enumerate(layer.neurons):
+        layerIdx = 0
+        for layer in self.layers:
+            neuronIdx = 0
+            for neuron in layer.neurons:
                 if (layerIdx == 0):
-                    for weightIdx, inpWeight in enumerate(neuron.inpWeights):
+                    weightIdx = 0
+                    for inpWeight in neuron.inpWeights:
                         print(inpWeight)
                         neuron.output += inpWeight * dataPoint[weightIdx] + neuron.bias
-                    neuron.output = sigmoid(neuron.output)
+                        weightIdx += 1
+                    neuron.output = layer.activation(neuron.output)
                     print("Layer {}, Neuron {}; OUTPUT = {}".format(layerIdx, neuronIdx, neuron.output))
-
                 else:
-                    for weightIdx, inpWeight in enumerate(neuron.inpWeights):
+                    weightIdx = 0
+                    for inpWeight in neuron.inpWeights:
                         print(inpWeight)
                         neuron.output += inpWeight * self.layers[layerIdx - 1].neurons[weightIdx].output + neuron.bias
-                    neuron.output = sigmoid(neuron.output)
+                        weightIdx += 1
+                    neuron.output = layer.activation(neuron.output)
                     print("Layer {}, Neuron {}; OUTPUT = {}".format(layerIdx, neuronIdx, neuron.output))
+                    neuronIdx += 1
+            layerIdx += 1
 
-    def bckProp(self, targetVec, learnRate):
-        print("temp")
+    def bckProp(self, targetVec, learnRate, dataPoint):
+        def derivative(layerIdx, output):
+            if self.layers[layerIdx].activation == sigmoid:
+                return output * (1 - output)
+            if self.layers[layerIdx].activation == relu:
+                if output > 0:
+                    return 1
+                else:
+                    return 0
+
+        def updateDeltas():
+            neuronIdx = 0
+            for neuron in self.layers[-1].neurons:
+                coefficient = targetVec[neuronIdx] - neuron.output
+                neuron.delta = derivative(-1, neuron.output) * coefficient
+                neuronIdx += 1
+            layerIdx = self.hiddenLayers - 1
+            while layerIdx >= 0:
+                neuronIdx = 0
+                for neuron in self.layers[layerIdx].neurons:
+                    coefficient = 0
+                    for prevNeuron in self.layers[layerIdx + 1].neurons:
+                        coefficient += prevNeuron.delta * prevNeuron.inpWeights[neuronIdx]
+                    neuron.delta = derivative(layerIdx, neuron.output) * (coefficient)
+                    neuronIdx += 1
+                layerIdx -= 1
+
+        def updateWeights():
+            layerIdx = self.hiddenLayers
+            while layerIdx >= 1:
+                neuronIdx = 0
+                for neuron in self.layers[layerIdx].neurons:
+                    weightIdx = 0
+                    for weight in neuron.inpWeights:
+                        weight += (learnRate * neuron.delta * self.layers[layerIdx - 1].neurons[weightIdx].output)
+                        weightIdx += 1
+                    neuronIdx += 1
+                layerIdx -= 1
+            for neuron in self.layers[layerIdx].neurons:
+                weightIdx = 0
+                for weight in neuron.inpWeights:
+                    weight += (learnRate * neuron.delta * dataPoint[weightIdx])
+                    weightIdx += 1
+                neuronIdx += 1
+
+        updateDeltas()
+        updateWeights()
+
         # Datasets are passed as a parameter to the train/test function.
         # This allows for networks to be created independent of datasets
         # but demands for initialization of the number of weights on
         # first-layer neurons upon first introducing a dataset.
+
     def train(self, data, targets, learnRate):
         if (self.initialized == False):
             initData(data)
-        if (len(data[0]) != self.firstLayer.neurons[0].inpWeights):
-            print("Data format incompatible: number of inputs do not match.")
+        if (len(data[0]) != len(self.firstLayer.neurons[0].inpWeights)):
+            print("Data format incompatible: number of features do not match initial dataset.")
             return 0
-        for idx, dataPoint in enumerate(data):
+        idx = 0
+        for dataPoint in data:
             self.fwdProp(dataPoint)
-            self.bckProp(targets[idx], learnRate)
+            self.bckProp(targets[idx], learnRate, dataPoint)
+            idx += 1
+
     def test(self, data):
         if (self.initialized == False):
             self.initData(data)
@@ -89,12 +149,10 @@ class network:
             self.fwdProp(dataPoint)
 
 def main():
-    data = list(np.random.rand(3, 3))
-    firstNetwork = network(2, 3, 2)
-    print(data[2])
+    data = [[1, 4, 3], [2, 4, 6], [7, 8, 2]]
+    targets = [[1, 0], [0, 1], [1, 0]]
+    firstNetwork = network(hiddenLayers = 2, layerLength = 3, outputCnt = 2)
     firstNetwork.test(data)
-    data = list(np.random.rand(4, 3))
-    firstNetwork.test(data)
+    firstNetwork.train(data, targets, learnRate = 0.1)
 
-        
 main()
